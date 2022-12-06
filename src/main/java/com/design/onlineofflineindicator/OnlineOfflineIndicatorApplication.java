0f183916c6389aec573463036eb0f1958ca4e220
sudhanshu179 <8004279014sS>
2022-12-06 22:40:11 +0530

    init

---
+++ src/main/java/com/design/onlineofflineindicator/OnlineOfflineIndicatorApplication.java
@@ -0,0 +1,23 @@
+package com.design.onlineofflineindicator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.export.datadog.DatadogMetricsExportAutoConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude = {DatadogMetricsExportAutoConfiguration.class, DataSourceAutoConfiguration.class})
+public class OnlineOfflineIndicatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineOfflineIndicatorApplication.class, args);
+    }
+
+}
